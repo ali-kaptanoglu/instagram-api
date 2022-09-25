76f9babafffefe0f9957a1e06678090601683574
ali-kaptanoglu <90628814+ali-kaptanoglu@users.noreply.github.com>
2022-09-25 22:31:38 +0300

    Create search.php

---
+++ search.php
@@ -0,0 +1,34 @@
+<?php
+$ch = curl_init();
+ 
+curl_setopt($ch, CURLOPT_URL, 'https://www.instagram.com/web/search/topsearch/?query=instagram');
+curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+curl_setopt($ch, CURLOPT_CUSTOMREQUEST, 'GET');
+curl_setopt($ch, CURLOPT_SSL_VERIFYHOST, 0);
+curl_setopt($ch, CURLOPT_SSL_VERIFYPEER, 0);
+ 
+$headers = array();
+$headers[] = 'Authority: www.instagram.com';
+$headers[] = 'Pragma: no-cache';
+$headers[] = 'Cache-Control: no-cache';
+$headers[] = 'Upgrade-Insecure-Requests: 1';
+$headers[] = 'User-Agent: Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/85.0.4183.102 Safari/537.36';
+$headers[] = 'Accept: text/html,application/xhtml+xml,application/xml;q=0.9,image/avif,image/webp,image/apng,*/*;q=0.8,application/signed-exchange;v=b3;q=0.9';
+$headers[] = 'Sec-Fetch-Site: cross-site';
+$headers[] = 'Sec-Fetch-Mode: navigate';
+$headers[] = 'Sec-Fetch-User: ?1';
+$headers[] = 'Sec-Fetch-Dest: document';
+$headers[] = 'Accept-Language: tr-TR,tr;q=0.9,en-US;q=0.8,en;q=0.7';
+$headers[] = 'Cookie: İnstagram Cookie Bilgileriniz';
+ 
+curl_setopt($ch, CURLOPT_HTTPHEADER, $headers);
+ 
+$result = curl_exec($ch);
+if (curl_errno($ch)) {
+echo 'Error:' . curl_error($ch);
+}
+curl_close($ch);
+echo $result;
+ 
+ 
+?>
